8fbc83327a1bcc945c2109eb97b1bde2a610222c
emanuel <s.paz@escolar.ifrn.edu.br>
2026-09-16 17:56:11 -0300

    exemplo 5

--- js/app1.js
+++ js/app1.js
@@ -26,10 +26,58 @@ ant.addEventListener("click", anterior);
 
 function selecionar(e){
     e.preventDefault();
-    i = Number(cbbox.value)\;
+    i = Number(cbbox.value);
     imagem.setAttribute("src",`./img/foto${i}.jpg`);
 }
 
 const cbbox = document.getElementById("combobox");
 cbbox.addEventListener("change", selecionar);
 
+const opacidade = document.getElementById("opacidade");
+opacidade.addEventListener("input", function(e){
+    e.preventDefault();
+    let valor = opacidade.value;
+    foto.setAttribute("style", `opacity:${valor}`);
+});
+
+const li1 = document.getElementById("li1");
+li1.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 1;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+})
+
+const li2 = document.getElementById("li2");
+li2.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 2;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+})
+
+const li3 = document.getElementById("li3");
+li2.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 3;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+})
+
+const li4 = document.getElementById("li4");
+li2.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 4;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+})
+
+const li5 = document.getElementById("li5");
+li2.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 5;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+})
+
+const li6 = document.getElementById("li6");
+li2.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 5;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+})
