a222bb2209121ec99ad2367bea1432127301f0fc
emanuel <s.paz@escolar.ifrn.edu.br>
2026-09-16 17:15:09 -0300

    exemplo change

--- js/app1.js
+++ js/app1.js
@@ -23,3 +23,13 @@ function anterior(e){
 }
 const ant = document.getElementById("ant");
 ant.addEventListener("click", anterior);
+
+function selecionar(e){
+    e.preventDefault();
+    i = Number(cbbox.value)\;
+    imagem.setAttribute("src",`./img/foto${i}.jpg`);
+}
+
+const cbbox = document.getElementById("combobox");
+cbbox.addEventListener("change", selecionar);
+
